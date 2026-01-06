625c97b88b485a8a7fd1ea107e09ac845a9d6539
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-06 20:07:25 +0100

    update

---
+++ Cs.Advanced.AnonymousMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Cs.Advanced.AnonymousMethods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Subscriber subscriber = new Subscriber();
+        Publisher publisher = new Publisher();
+
+        //handle the event (or) subscribe to event
+        publisher.myEvent += delegate (int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        };
+
+        //invoke the event
+        publisher.RaiseEvent(10, 20);
+        publisher.RaiseEvent(5,80);
+        publisher.RaiseEvent(14,22);
+
+        Console.ReadKey();
+    }
+}
---
+++ Cs.Advanced.AnonymousMethods/Publisher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cs.Advanced.AnonymousMethods;
+
+//delegate type
+public delegate void MyDelegateType(int a, int b);
+
+public class Publisher
+{
+    //step 1: create event
+    public event MyDelegateType myEvent;
+
+    public void RaiseEvent(int a, int b) {
+        //step 2: raise event
+        if (this.myEvent != null) { 
+            this.myEvent(a, b);
+        }
+    }
+
+}
